d99776a6d820dbb1a28e07f868a8e5e9f4785547
Amrrish1209 <roshanbabubl@gmail.com>
2023-06-02 12:33:58 +0530

    Add: Extend the Servlet to accept a valid Name

--- src/main/java/com/bridgelabz/firstservletproject/LoginServlet.java
+++ src/main/java/com/bridgelabz/firstservletproject/LoginServlet.java
@@ -11,34 +11,45 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(
-		description="Login Servlet Testing",
-		urlPatterns= {"/LoginServlet"},
-		initParams= {
-				@WebInitParam(name="user",value="Amrrish"),
-				@WebInitParam(name="password",value="Roshan")
-		}
-		)
-
-
+@WebServlet(description = "Login Servlet Testing", urlPatterns = { "/LoginServlet" }, initParams = {
+		@WebInitParam(name = "user", value = "Amrrish"), @WebInitParam(name = "password", value = "Roshan") })
 public class LoginServlet extends HttpServlet {
 
 	@Override
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException{
-		//get request parameters for userID and password
-		String user=request.getParameter("user");
-		String pwd=request.getParameter("pwd");
-		//get servlet config init params
-		String userID=getServletConfig().getInitParameter("user");
-		String password=getServletConfig().getInitParameter("password");
-		if(userID.equals(user)&& password.equals(pwd)) {
-			request.setAttribute("user", user);
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// Get request parameters for username and password
+		String username = request.getParameter("user");
+		String password = request.getParameter("pwd");
+
+		// Get servlet config init parameters
+		String userID = getServletConfig().getInitParameter("user");
+		String userPassword = getServletConfig().getInitParameter("password");
+
+		// Validate the username
+		boolean isUsernameValid = isValidUsername(username);
+
+		if (isUsernameValid && userID.equals(username) && userPassword.equals(password)) {
+			request.setAttribute("user", username);
 			request.getRequestDispatcher("LoginSuccess.jsp").forward(request, response);
-		}else {
-			RequestDispatcher rd=getServletContext().getRequestDispatcher("/login.html");
-			PrintWriter out=response.getWriter();
-			out.println("<font color=red>Either user name or password is wrong.<font>");
+		} else {
+			RequestDispatcher rd = getServletContext().getRequestDispatcher("/login.html");
+			PrintWriter out = response.getWriter();
+			if (!isUsernameValid) {
+				out.println(
+						"<font color=red>Invalid username. Name should start with a capital letter and have a minimum of 3 characters.<font>");
+			} else {
+				out.println("<font color=red>Incorrect username or password.<font>");
+			}
 			rd.include(request, response);
 		}
+	}
+
+	// Method to validate the username
+	private boolean isValidUsername(String username) {
+		if (username.length() >= 3 && Character.isUpperCase(username.charAt(0))) {
+			return true;
+		}
+		return false;
 	}
 }
